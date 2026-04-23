abca0d1a17c2f2e3c5e70bf263682d3cc004998b
rickyyn <ritsccd@gmail.com>
2026-04-23 17:32:27 -0300

    agr vai

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,12 @@ android {
     }
 }
 
+ksp {
+    arg("room.generateKotlin", "true")
+}
+
 dependencies {
-    val room_version = "2.6.1"
+    val room_version = "2.7.0-alpha11"
     implementation("androidx.room:room-runtime:$room_version")
     implementation("androidx.room:room-ktx:$room_version")
     ksp("androidx.room:room-compiler:$room_version")
--- app/src/main/java/br/edu/fatecpg/myalejandro/dao/UsuarioDao.kt
+++ app/src/main/java/br/edu/fatecpg/myalejandro/dao/UsuarioDao.kt
@@ -1,12 +1,15 @@
 package br.edu.fatecpg.myalejandro.dao
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import br.edu.fatecpg.myalejandro.model.Usuario
 
+@Dao
 interface UsuarioDao {
     @Query("SELECT * FROM usuario")
-    fun buscarTodosUsuarios():List<Usuario>
+    suspend fun buscarTodosUsuarios(): List<Usuario>
+
     @Insert
-    suspend fun inserirUsuario(vararg usuario: Usuario)
+    suspend fun inserirUsuario(usuario: Usuario)
 }
